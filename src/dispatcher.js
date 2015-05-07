9f3b40b7e462a69d4598340b7fc38cbc7bae487d
Ed Moore <eddie.moore@gmail.com>
2015-05-07 14:18:16 +0800

    initial commit

---
+++ src/dispatcher.js
@@ -0,0 +1,39 @@
+let listeners = {}
+
+function register (event, fn) {
+  listeners[event] = listeners[event] || []
+  listeners[event].push(fn)
+}
+
+function remove (event, fn) {
+  let handlers = listeners[event]
+
+  handlers.splice(handlers.indexOf(fn), 1)
+}
+
+function dispatch (event, ...args) {
+  let eventListeners = listeners[event]
+  if (eventListeners) {
+    eventListeners.forEach(event => {
+      event.apply(null, args)
+    })
+  }
+}
+
+function getEventHandlers (event) {
+  return listeners[event]
+}
+
+function clearEventHandlers (event) {
+  if (event) {
+    listeners[event] = []
+  }
+}
+
+export default {
+  register,
+  remove,
+  dispatch,
+  getEventHandlers,
+  clearEventHandlers
+}
